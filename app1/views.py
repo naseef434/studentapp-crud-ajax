bbd1a2c817427fbfe3614d8a9c9c93ad3a5274e1
naseef <naseefp.hin@gmail.com>
2021-04-30 02:50:07 +0530

    student data inserted

--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import Student
+from django.core import serializers
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'student.html')
+    if request.method == "POST":
+        name = request.POST['name']
+        number = request.POST['number']
+        student_create = Student.objects.create(name=name,number=number)
+        data = {'student_create':serializers.serialize('json',[student_create])}
+        return JsonResponse(data)
+    else:
+        student_details = Student.objects.all()
+        context = {'student_details':student_details}
+        return render(request, 'student.html',context)
